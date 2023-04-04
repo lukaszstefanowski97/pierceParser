4c4e10dc226de07167a1ac0536f14c1befce835f
Łukasz Stefanowski <lukasz.stefanowski@lamin.ar>
2023-04-04 13:15:06 +0200

    added unit tests

---
+++ src/test/java/com/example/pierceParser/AttributeControllerTest.java
@@ -0,0 +1,60 @@
+package com.example.pierceParser;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class AttributeControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getAllAttributesTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/attributes")
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "pl_PL")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getAttributeByCodeTest_success() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/attributes/helmetsize")
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "pl_PL")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getAttributeByCodeTest_notFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/attributes/attributeThatDoesNotExist")
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "pl_PL")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
---
+++ src/test/java/com/example/pierceParser/OptionsControllerTest.java
@@ -0,0 +1,78 @@
+package com.example.pierceParser;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class OptionsControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getAllOptionsTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/options")
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "pl_PL")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getOptionByCodeTest_success() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/options/9_10")
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "pl_PL")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getOptionByCodeTest_notFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/options/codeThatDoesNotExist")
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "pl_PL")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getOptionByAttribute_success() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/options/byAttribute/helmetsize")
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "pl_PL")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getOptionByAttribute_notFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/options/byAttribute/attributeThatDoesNotExist")
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "pl_PL")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+}
